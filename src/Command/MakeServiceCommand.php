e8b846402abd3cc5924b1865c30ea248b5d1ec6e
Developer <developer@developer.com>
2023-04-04 22:19:47 +0700

    Add path controller

--- src/Command/MakeServiceCommand.php
+++ src/Command/MakeServiceCommand.php
@@ -34,21 +34,23 @@ class MakeServiceCommand extends Command
     private bool $isCreateResource = true;
     private bool $isCreateModel = true;
     private bool $isCreateService = true;
+    private string $path;
 
     /**
      * Execute the console command.
      *
      * @return int
      */
-    public function handle()
+    public function handle ()
     {
         $this->modelName = $this->argument('modelName');
-        $this->info("Model Name: ".$this->modelName);
+        $this->info("Model Name: " . $this->modelName);
         $this->isCreateController = !$this->option('no-controller');
         $this->isCreateRequest = !$this->option('no-request');
         $this->isCreateModel = !$this->option('no-model');
         $this->isCreateService = !$this->option('no-service');
         $this->isCreateResource = !$this->option('no-resource');
+        $this->path = $this->option('path');
 
         $this->createController();
         $this->createRequest();
@@ -59,83 +61,92 @@ class MakeServiceCommand extends Command
         return Command::SUCCESS;
     }
 
-    private function createController(){
-        if($this->isCreateController){
+    private function createController ()
+    {
+        if ($this->isCreateController) {
             $stub = $this->getStub('Controller');
             $replacement = [
-                '{ModelName}' => $this->modelName,
-                '{lineRequest}'=> $this->isCreateRequest ? '$this->request'." = \App\Http\Requests\\".$this->modelName."Request::class;" : '',
-                '{lineResource}' => $this->isCreateResource ? '$this->resource'." = \App\Http\Resources\\".$this->modelName."Resource::class;" : ''
+                '{Path}'         => $this->path ?? '',
+                '{ModelName}'    => $this->modelName,
+                '{lineRequest}'  => $this->isCreateRequest ? '$this->request' . " = \App\Http\Requests\\" . $this->modelName . "Request::class;" : '',
+                '{lineResource}' => $this->isCreateResource ? '$this->resource' . " = \App\Http\Resources\\" . $this->modelName . "Resource::class;" : ''
             ];
-            $stub = $this->replaceStub($stub,$replacement);
+            $stub = $this->replaceStub($stub, $replacement);
             $newFileName = "{$this->modelName}Controller.php";
-            $path = app_path()."/Http/Controllers/".$newFileName;
+            $_path = $this->path ? $this->path . "/" . $newFileName : $newFileName;
+            $path = app_path() . "/Http/Controllers/" . $_path;
             file_put_contents($path, $stub);
             $this->info("Success create controller at {$path}");
         }
     }
 
-    private function createRequest(){
-        if($this->isCreateRequest){
+    private function createRequest ()
+    {
+        if ($this->isCreateRequest) {
             $stub = $this->getStub('Request');
             $replacement = [
                 '{ModelName}' => $this->modelName
             ];
-            $stub = $this->replaceStub($stub,$replacement);
+            $stub = $this->replaceStub($stub, $replacement);
             $newFileName = "{$this->modelName}Request.php";
-            $path = app_path()."/Http/Requests/".$newFileName;
+            $path = app_path() . "/Http/Requests/" . $newFileName;
             file_put_contents($path, $stub);
             $this->info("Success create request at {$path}");
         }
     }
 
-    private function createResource(){
-        if($this->isCreateResource){
+    private function createResource ()
+    {
+        if ($this->isCreateResource) {
             $stub = $this->getStub('Resource');
             $replacement = [
                 '{ModelName}' => $this->modelName
             ];
-            $stub = $this->replaceStub($stub,$replacement);
+            $stub = $this->replaceStub($stub, $replacement);
             $newFileName = "{$this->modelName}Resource.php";
-            $path = app_path()."/Http/Resources/".$newFileName;
+            $path = app_path() . "/Http/Resources/" . $newFileName;
             file_put_contents($path, $stub);
             $this->info("Success create resource at {$path}");
         }
     }
 
-    private function createModel(){
-        if($this->isCreateModel){
+    private function createModel ()
+    {
+        if ($this->isCreateModel) {
             $stub = $this->getStub('Model');
             $replacement = [
                 '{ModelName}' => $this->modelName
             ];
-            $stub = $this->replaceStub($stub,$replacement);
+            $stub = $this->replaceStub($stub, $replacement);
             $newFileName = "{$this->modelName}.php";
-            $path = app_path()."/Models/".$newFileName;
+            $path = app_path() . "/Models/" . $newFileName;
             file_put_contents($path, $stub);
             $this->info("Success create model at {$path}");
         }
     }
 
-    private function createService(){
-        if($this->isCreateService){
+    private function createService ()
+    {
+        if ($this->isCreateService) {
             $stub = $this->getStub('Service');
             $replacement = [
                 '{ModelName}' => $this->modelName
             ];
-            $stub = $this->replaceStub($stub,$replacement);
+            $stub = $this->replaceStub($stub, $replacement);
             $newFileName = "{$this->modelName}Service.php";
-            $path = app_path()."/Services/".$newFileName;
+            $path = app_path() . "/Services/" . $newFileName;
             file_put_contents($path, $stub);
             $this->info("Success create service at {$path}");
         }
     }
 
-    private function getStub($file){
+    private function getStub ($file)
+    {
         return file_get_contents(__DIR__ . "/../../stubs/${file}.stub");
     }
 
-    private function replaceStub($stub,$replacements){
+    private function replaceStub ($stub, $replacements)
+    {
         $stub = str_replace(array_keys($replacements), array_values($replacements), $stub);
         return $stub;
     }
